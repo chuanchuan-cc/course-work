5a0869a0cd35f35746a37e0658f79999b32815ae
LAPTOP-U54VG72G\1 <minyuz074@gmail.com>
2025-04-09 00:50:37 +0100

    轮船偏移好了一点点，station和utility在地图中只保留名字

--- Assets/Scripts/CGcontrol.cs
+++ Assets/Scripts/CGcontrol.cs
@@ -1,10 +1,6 @@
 using UnityEngine;
 using System.Collections;
 using System.Numerics;
-using System.Collections.Generic;
-using System;
-using JetBrains.Annotations;
-
 
 public class CGcontrol : MonoBehaviour
 {
@@ -69,71 +65,7 @@ public class CGcontrol : MonoBehaviour
 
     }
 
-    // 当棋子走到特殊格子时调用此方法
-    public void TriggerSpecialTileAnimation(string cgName)
-    {
-        // string animationName = "";
-        
-        // 根据格子类型确定要播放的动画名称
-        // switch (animationName)
-        // {
-        //     case "Bank_add":
-        //         animationName = "wallet_add";
-        //         break;
-
-        //     case "Bank_lose":
-        //         animationName = "wallet_lose";
-        //         break;
-
-        //     case "Jail":
-        //         animationName = "jail"; // 替换为实际的Jail动画名称
-        //         break;
-                
-        //     default:
-        //         Debug.LogWarning("Unknown tile type: " + cgName);
-        //         return;
-        // }
-
-        // 在屏幕中央播放动画
-        StartCoroutine(CenterAnimation(cgName));
-    }
-    
-    private IEnumerator CenterAnimation(string cgName)
-    {
-        // 查找动画对象
-        Transform animTransform = behaviorCG.transform.Find(cgName);
-        if (animTransform == null)
-        {
-            Debug.LogWarning("Can't find the animation object: " + cgName);
-            yield break;
-        }
-
-        GameObject animObject = animTransform.gameObject;
-        
-        // 设置动画位置为屏幕中央
-        animObject.transform.position = new UnityEngine.Vector3(Screen.width / 2, Screen.height / 2, 0);
-        animObject.SetActive(true);
-
-       // 获取动画组件并播放
-        Animator animator = animObject.GetComponent<Animator>();
-        if (animator != null)
-        {
-            animator.Play(cgName);
-            
-            // 等待动画播放完成
-            yield return new WaitForSeconds(animator.GetCurrentAnimatorStateInfo(0).length);
-        }
-        else
-        {
-            Debug.LogWarning("No Animator component found on: " + cgName);
-            // 如果没有动画组件，默认显示5秒
-            yield return new WaitForSeconds(5f);
-        }
-        
-        animObject.SetActive(false);
-    }
-
-    }
+}
 
 
 
